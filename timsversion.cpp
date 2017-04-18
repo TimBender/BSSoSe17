fe8bf3773f07884ca37a9157688a6ba119625d7a
Tim Bender <TimBender@users.noreply.github.com>
2017-04-18 23:50:12 +0200

    Create timsversion.cpp
    nur mal so n test, man muss das dann halt kopieren in ein eigenes projekt in die main :P

---
+++ timsversion.cpp
@@ -0,0 +1,80 @@
+#include <cstdlib>
+#include <stdio.h>
+#include <unistd.h>
+#include <sstream>
+#include <sys/types.h>
+#include <string.h>
+#include <iostream>
+#include <signal.h>
+#include <wait.h>
+#include <time.h> 
+#include <ctime>
+
+using namespace std;
+
+#define BUFFER_BIT 510
+
+int main(int argc, char** argv) {
+    
+    int status;
+    
+    /*Pipe erstellen*/
+    int fd[2];
+    pipe(fd); // in C automatisch Ref zum 1.obj
+
+    char input = ' ';
+    /*Kopie von sich selbst erstellen*/
+    int pm_id;
+    char command[BUFFER_BIT];
+
+    clock_t tstart = clock(); // start 
+    while (true) {
+
+        switch (pm_id = fork()) {
+            case -1:
+                cerr << "ERROR_forking failed.\n";
+                break;
+
+            case 0: /*child*/
+            {
+                cin>>command;
+                /*write to pipe*/
+                write(fd[1], command, BUFFER_BIT);
+
+                read(fd[0], command, BUFFER_BIT);
+
+                string n = command;
+                if (n == "Q" || n == "Quit") {
+                    tstart = clock() - tstart;
+                    cout << "Durchlaufzeit: " << ((float)tstart) / CLOCKS_PER_SEC << '\n';
+                    kill(getppid(), SIGKILL);
+                }
+                if (n == "P" || n == "Print") {
+                    int rep_id = fork();
+
+                    switch (rep_id) {
+                        case 0: //Reporter
+                            cout << "RUNNING PROCESS:\n";
+                            system("ps aux | less"); //running processes
+                            //system("ps -o pid,pgid,time,etime,group,ppid,user,vsz,cmd");
+                            exit(0);
+                            break;
+                        default:
+                            wait(0);
+                            break;
+                    }
+                }
+            }
+                break;
+
+            default: /*parent*/
+            {
+                wait(&status);
+                break;
+            }
+        }
+        cout << "\n";
+    }
+    exit(0);
+    return 0;
+}
