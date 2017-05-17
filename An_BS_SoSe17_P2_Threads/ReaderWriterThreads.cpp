c531c5aa80b0c89cd1d573a4c264a98a4d86b3bb
An_Tr <an.hat.tr@gmail.com>
2017-05-17 19:46:47 +0200

    Version mit fairer Behandlung der Threads: siehe Methoden in ReaderWriter Thread cpp

--- An_BS_SoSe17_P2_Threads/ReaderWriterThreads.cpp
+++ An_BS_SoSe17_P2_Threads/ReaderWriterThreads.cpp
@@ -9,11 +9,14 @@
 #include <chrono>			// for std::chrono... 
 #include<mutex>
 
-sem_t mutex = 1;
-sem_t db = 1;
+/* READER PREFERENCE
+=================================================================================*/
+
+/*semaphores for reader preference solution*/
+sem_t mutex;
+sem_t db;
 int rc = 0;
 
-// ******** reader & writer threads ******** 
 // The writer thread
 void writer(int writerID, int numSeconds) 
 {
@@ -73,7 +76,7 @@ void reader(int readerID, int numSeconds) {
 	
 		sem_wait(&mutex); //exklusiver Zugriff auf reader counter
 		rc--;
-		if (rc == 0) sem_post(&mutex); //unlock DB, when last reader thread has finished
+		if (rc == 0) sem_post(&db); //unlock DB, when last reader thread has finished
 		sem_post(&mutex);
 
 		// sleep a while...
@@ -93,6 +96,99 @@ void reader(int readerID, int numSeconds) {
 		<< " finished. ==\nReturning after " << tests
 		<< " tests." << std::endl << std::endl;
 
-} // end reader function
+} 
 
+/* FAIR TREATMENT: no thread starvation pursued, add FIFO scheduling
+=================================================================================*/
 
+/*semaphores for fair treatment*/
+sem_t fair_mutex;							//for fair_rc  
+sem_t fair_db;								//access control on database
+sem_t fifoQueue;							//preserves right order
+int fair_rc=0;								//reader counter
+
+// The writer thread
+void fairWriter(int writerID, int numSeconds)
+{
+	std::cout << "Fair writer " << writerID << " starting..." << std::endl << std::endl;
+
+	int	tests = 0;
+	auto startTime = std::chrono::steady_clock::now();
+	std::chrono::seconds maxTime(numSeconds);
+
+	//timeslot not exhausted 
+	while ((std::chrono::steady_clock::now() - startTime) < maxTime)
+	{
+		sem_wait(&fifoQueue);	//wait to be served
+		sem_wait(&fair_db);		//exclusive access to db
+		sem_post(&fifQueue);
+
+		bool result = theDatabase.write(writerID); //write to db
+
+		sem_post(&db); //release exclusive access to db
+
+		++tests;
+
+		// sleep a while...
+		//int numSeconds2sleep = randomInt(3); // i.e. either 0, 1 or 2 
+		int numSeconds2sleep = 1;
+		//std::chrono::seconds randomSeconds(numSeconds2sleep);
+		std::cout << "WRITER " << writerID << '\n';
+		std::cout << "_________________________________\n"
+			<< "finished test " << tests << '\n'
+			<< "result = " << (result == true ? "correct" : "incorrect") << '\n'
+			<< "sleeping " << numSeconds2sleep
+			<< " seconds " << "\n_________________________________\n\n";
+		//if (numSeconds2sleep) std::this_thread::sleep_for(randomSeconds);
+		std::this_thread::sleep_for(std::chrono::seconds(numSeconds2sleep));
+	} // repeat until time used is up
+
+	std::cout << "\n== WRITER " << writerID
+		<< " finished == \n" << "Returning after: " << tests
+		<< " tests." << std::endl << std::endl;
+
+}
+
+// The reader thread
+void fairReader(int readerID, int numSeconds) {
+
+	std::cout << "Fair reader " << readerID << " starting..." << std::endl << std::endl;
+
+	int	tests = 0;
+	auto startTime = std::chrono::steady_clock::now();
+	std::chrono::seconds maxTime(numSeconds);
+	while ((std::chrono::steady_clock::now() - startTime) < maxTime)
+	{
+		sem_wait(&fifoQueue);	//wait in line to be served
+		sem_wait(&fair_mutex);	//exklusiver Zugriff fuer reader counter/rc
+		rc++;
+		if (fair_rc == 1) sem_wait(&fair_db);//shared_mutex_db.lock(); //1.Reader => lock DB
+		sem_post(&fair_mutex);
+
+		//Read from database
+		bool result = theDatabase.read(readerID);
+		++tests;
+
+		sem_wait(&fair_mutex); //exklusiver Zugriff auf reader counter
+		rc--;
+		if (fair_rc == 0) sem_post(&fair_db); //unlock DB, when last reader thread has finished
+		sem_post(&fair_mutex);
+
+		// sleep a while...
+		//int numSeconds2sleep = randomInt(3); // i.e. either 0, 1 or 2 
+		int numSeconds2sleep = 1;
+		//std::chrono::seconds randomSeconds(numSeconds2sleep);
+		std::cout << "READER " << readerID << "\n________________________________________\n"
+			<< "finished test " << tests << '\n'
+			<< "result = " << (result == true ? "correct" : "incorrect") << '\n'
+			<< "sleeping " << numSeconds2sleep
+			<< " seconds " << "\n________________________________________\n\n";
+		std::this_thread::sleep_for(std::chrono::seconds(numSeconds2sleep));
+
+	} // repeat until time is used up
+
+	std::cout << "\n== READER " << readerID
+		<< " finished. ==\nReturning after " << tests
+		<< " tests." << std::endl << std::endl;
+
+}
--- An_BS_SoSe17_P2_Threads/ReaderWriterThreads.h
+++ An_BS_SoSe17_P2_Threads/ReaderWriterThreads.h
@@ -1,9 +1,23 @@
 #pragma once
 
+
+/* READER WRITER PROBLEM: reader preference
+=========================================*/
+
 // The writer thread
 void writer(int writerID, int numSeconds);
 
 // The reader thread
 void reader(int readerID, int numSeconds);
+
+
+/* READER WRITER PROBLEM: fair treatment
+=========================================*/
+
+// The writer thread
+void fairWriter(int writerID, int numSeconds);
+
+// The reader thread
+void fairReader(int readerID, int numSeconds);
 
 
